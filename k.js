45d16be49bcbd538425324759266b32304fab685
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-09-29 13:42:10 +0300

    Фикс

--- k.js
+++ k.js
@@ -8,6 +8,7 @@ class Car {
         console.log(`Машина марки ${this.marca}, ей ${this.age} лет.`)
     }
 }
+console.log("___")
 
 let ford = new Car("Ford", 10);
 ford.ccc()
@@ -17,9 +18,19 @@ console.log("___")
 let doodge = new Car("Doodge", 6);
 doodge.ccc();
 
+console.log("___")
+
 let toyota = new Car("Toyota", 18);
 toyota.ccc();
+
+console.log("___")
 
 let nissan = new Car("Nissan", 11);
 nissan.ccc();
 
+console.log("___")
+
+let infinity = new Car("Infinity", 2);
+infinity.ccc();
+
+console.log("___")
