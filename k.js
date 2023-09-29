9cbb1e93593e9502b98ca9d88667522b220dedde
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-09-29 12:25:13 +0300

    Первая фиксация

---
+++ k.js
@@ -0,0 +1,13 @@
+class Car {
+    constructor(marca, age){
+        this.marca = marca,
+        this.age = age
+    }
+
+    ccc(){
+        console.log(`Машина марки ${this.marca}, ей ${this.age} лет.`)
+    }
+}
+
+let ford = new Car("Ford", 10);
+ford.ccc()
